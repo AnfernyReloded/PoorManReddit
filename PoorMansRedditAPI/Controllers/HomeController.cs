9ab9ccca9d6e8c4c35e619326c37700ba4a6cc24
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-07-29 22:06:41 -0400

    I hate git bash, I want to go to bed

---
+++ PoorMansRedditAPI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PoorMansRedditAPI.Models;
+
+namespace PoorMansRedditAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RedditDAL _reddit = new RedditDAL();
+
+        public HomeController() { }
+
+        public async Task<ActionResult> Index()
+        {
+            var reddit = await _reddit.GetImageBySearchTerm();
+            return View(reddit);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+       
+
+
+    }
+}
---
+++ PoorMansRedditAPI/Models/RedditDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PoorMansRedditAPI.Models
+{
+    public class RedditDAL
+    {
+        private HttpClient GetHttpClient() 
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.reddit.com");
+            return client;
+        
+        }
+
+        public async Task<RedditObject> GetImageBySearchTerm() 
+        {
+            var client = GetHttpClient();
+            var request = await client.GetAsync($"/r/aww/.json");
+            //var request = await client.GetAsync($"{client.BaseAddress}");
+            var response = await request.Content.ReadAsAsync<RedditObject>();
+
+            return response;
+        }
+    }
+}
